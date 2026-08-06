4ef1acb9ef556118c82af245d9d4792e64edf6c7
Charlie Starling <charliejackstarling1@gmail.com>
2026-08-06 02:54:24 +0100

    numbered for planning

--- cv_online/src/Grid.jsx
+++ cv_online/src/Grid.jsx
@@ -12,12 +12,12 @@ function Grid() {
                 <div id="div3" className="content-grid">3</div>
                 <div id="div4" className="content-grid">4</div>
                 <div id="div7" className="content-grid">7</div>
-                <div id="div8" className="content-grid">8</div>
-                <div id="div9" className="content-grid">9</div>
-                <div id="div11" className="content-grid">11</div>
-                <div id="div12" className="content-grid">12</div>
-                <div id="div15" className="content-grid">15</div>
-                <div id="div16" className="content-grid">16</div>
+                <div id="div8" className="content-grid">5</div>
+                <div id="div9" className="content-grid">6</div>
+                <div id="div11" className="content-grid">8</div>
+                <div id="div12" className="content-grid">9</div>
+                <div id="div15" className="content-grid">10</div>
+                <div id="div16" className="content-grid">11</div>
             </div>
 
         </>
