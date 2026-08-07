d60b509ac12f0120990f073a6211112ab4e81665
Charlie Starling <charliejackstarling1@gmail.com>
2026-08-07 21:38:24 +0100

    Renamed div elements appropriately

--- cv_online/src/Grid.jsx
+++ cv_online/src/Grid.jsx
@@ -8,26 +8,32 @@ function Grid() {
         <>
 
             <div className="parent z-0">
-                <div id="div1" className="content-grid">
+                <div id="profile-div" className="content-grid">
                     <Profile />
                 </div>
-                <div id="hero1" className="content-grid">About / Education</div>
-                
-                <div id="div7" className="content-grid">Skills</div>
-                <div id="div11" className="content-grid">8</div>
-                <div id="div12" className="content-grid">9</div>
+                <div id="about-div" className="content-grid">About / Education</div>
 
-                <div id="div2" className="content-grid">Projects</div>
-                <div id ="div2a" className="content-grid">2a</div>
-                <div id ="div2b" className="content-grid">2b</div>
-                <div id ="div2c" className="content-grid">2c</div>
+                <div id="skills-div" className="content-grid">Skills</div>
+                <div id="skill-item-1" className="content-grid">1</div>
+                <div id="skill-item-2" className="content-grid">2</div>
+                <div id="skill-item-3" className="content-grid">3</div>
+                <div id="skill-item-4" className="content-grid">4</div>
+                <div id="skill-item-5" className="content-grid">5</div>
+                <div id="skill-item-6" className="content-grid">6</div>
 
-                <div id="div3" className="content-grid">What I'm working on</div>
-                <div id="div4" className="content-grid">4</div>
-                <div id="div8" className="content-grid">5</div>
-                <div id="div9" className="content-grid">6</div>
+                <div id="projects-div" className="content-grid">Projects</div>
+                <div id ="project-item-1" className="content-grid">1</div>
+                <div id ="project-item-2" className="content-grid">2</div>
+                <div id ="project-item-3" className="content-grid">3</div>
+                <div id ="project-item-4" className="content-grid">4</div>
                 
-                <div id="div15" className="content-grid">Contact</div>
+
+                <div id="working-div" className="content-grid">What I'm working on</div>
+                <div id="working-item-1" className="content-grid">1</div>
+                <div id="working-item-2" className="content-grid">2</div>
+                <div id="working-item-3" className="content-grid">3</div>
+                
+                <div id="contact-div" className="content-grid">Contact</div>
             </div>
 
         </>
